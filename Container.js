d22f979a52e8db5095361b9087b05e1502cbb7a1
yosep <yosepalexsander9@gmail.com>
2021-11-18 11:02:16 +0700

    done bottom navigation

--- Container.js
+++ Container.js
@@ -7,8 +7,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 //Import Bottom Tab Navigation
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 //Import Icon
+import { Ionicons } from "@expo/vector-icons";
 
 // Import Theme Native Base
 import { useTheme } from "native-base";
@@ -22,6 +24,36 @@ import IncDec from "./src/screens/incDec";
 const Stack = createStackNavigator();
 
 //Create Bottom Tab Navigation
+const Tab = createBottomTabNavigator();
+function CustomTab() {
+  const theme = useTheme();
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={({ route }) => ({
+        headerMode: "screen",
+        headerTintColor: "white",
+        headerStyle: { backgroundColor: theme.colors.primary["300"] },
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused ? "ios-home" : "ios-home-outline";
+          } else if (route.name === "Form") {
+            iconName = focused ? "ios-information-circle" : "ios-information-circle-outline";
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: theme.colors.primary["700"],
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
+      <Tab.Screen name="Home" component={Hello} />
+      <Tab.Screen name="Form" component={FormNativeBase} />
+    </Tab.Navigator>
+  );
+}
 
 // Create Component Bottom Tab Navigation
 
@@ -31,19 +63,12 @@ export default function Container() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerMode: "screen",
-          headerTintColor: "white",
-          headerStyle: { backgroundColor: theme.colors.primary["300"] },
-        }}
-      >
+      <Stack.Navigator>
         <Stack.Screen
-          name="Home"
-          component={Hello}
+          name="Main"
+          component={CustomTab}
           options={{
-            title: "Hello Screen",
+            headerShown: false,
           }}
         />
         <Stack.Screen
@@ -51,6 +76,9 @@ export default function Container() {
           component={IncDec}
           options={{
             title: "Increment Decrement",
+            headerMode: "screen",
+            headerTintColor: "white",
+            headerStyle: { backgroundColor: theme.colors.primary["300"] },
           }}
         />
       </Stack.Navigator>
--- src/screens/hello.js
+++ src/screens/hello.js
@@ -1,16 +1,10 @@
 import * as React from "react";
-import { Text, Box, Pressable } from "native-base";
+import { Text, Box, Pressable, theme } from "native-base";
 
 // Add Props in Hello({navigation})
 export default function Hello({ navigation }) {
   return (
-    <Box
-      bg="primary.400"
-      flex={1}
-      alignItems="center"
-      justifyContent="center"
-      p={10}
-    >
+    <Box bg="primary.400" flex={1} alignItems="center" justifyContent="center" p={10}>
       <Text fontFamily="body" fontWeight={400} fontStyle="italic" fontSize={30}>
         Life's too short
       </Text>
@@ -25,6 +19,14 @@ export default function Hello({ navigation }) {
           justifyContent: "center",
           borderRadius: 10,
           margin: 20,
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+
+          elevation: 10,
         }}
       >
         <Text color={{ color: "white" }}>Screen Increment and Decrement</Text>

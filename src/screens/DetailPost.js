6ac9eb4fed73ac4175aee99417a9b82f8ac5c54a
yosep <yosepalexsander9@gmail.com>
2021-11-18 11:48:32 +0700

    done axios

--- src/screens/DetailPost.js
+++ src/screens/DetailPost.js
@@ -1,34 +1,70 @@
+import axios from "axios";
 import React from "react";
-import {
-  View,
-  Text,
-  Button,
-  StyleSheet,
-  FlatList,
-  RefreshControl,
-} from "react-native";
+import { View, Text, Button, StyleSheet, FlatList, RefreshControl } from "react-native";
 import { ListItem, Avatar } from "react-native-elements";
 
 // Import Axios
 
 const PostDetail = (props) => {
   //init Props
+  const title = props.route.params.title;
+  const body = props.route.params.body;
+  const id = props.route.params.id;
 
   //Init State
+  const [comments, setComments] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
 
   // Create LifeCycle
-  //Function Exception
+  React.useEffect(() => {
+    //Function Exception
+    getComments();
+  }, []);
 
   // Create Function to fetch
+  const getComments = () => {
+    setIsLoading(true);
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+      .then((res) => {
+        setComments(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        alert(err);
+        setIsLoading(false);
+      });
+  };
 
   //   Create Component List
+  const _renderItem = ({ item }) => {
+    return (
+      <ListItem key={item.id.toString()} bottomDivider>
+        <ListItem.Content>
+          <ListItem.Title numberOfLines={1}>{item.title}</ListItem.Title>
+          <ListItem.Subtitle numberOfLines={2}>
+            {item.name} - {item.body}
+          </ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    );
+  };
   return (
     <View style={style.container}>
       <Text h2 style={{ fontWeight: "bold" }}>
-        This Is Post Detail
+        {title}
       </Text>
-      {/* Implement Axios Here */}
+      <Text style={{ marginTop: 20 }}>{body}</Text>
+      <Text style={{ marginTop: 20 }}>Comments</Text>
       {/* Render Component List */}
+      <FlatList
+        data={comments}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={_renderItem}
+        refreshing={isLoading}
+        onRefresh={getComments}
+        nestedScrollEnabled
+      />
     </View>
   );
 };
--- src/screens/Posts.js
+++ src/screens/Posts.js
@@ -1,29 +1,62 @@
-import React from "react";
-import { View, Text, Button, StyleSheet, FlatList } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, ScrollView, Text, Button, StyleSheet, FlatList } from "react-native";
 import { ListItem, Avatar } from "react-native-elements";
 
 // Import Axios
+import axios from "axios";
 
 const Posts = (props) => {
   //Init State
+  const [post, setPost] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   // Create LifeCycle
-  //Function Exception
+  useEffect(() => {
+    //Function Exception
+    getPost();
+  }, []);
 
   // Create Function to fetch
-
+  const getPost = () => {
+    setIsLoading(true);
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        setPost(res.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        alert(error);
+        setIsLoading(false);
+      });
+  };
   //   Create Component List
+  const _renderItem = ({ item }) => {
+    return (
+      <ListItem onPress={() => props.navigation.navigate("DetailPost", item)} key={item.id.toString()} bottomDivider>
+        <Avatar rounded title={item.title.slice(0, 2)} containerStyle={{ backgroundColor: "black" }} />
+        <ListItem.Content>
+          <ListItem.Title h4 numberOfLines={1}>
+            {item.title}
+          </ListItem.Title>
+          <ListItem.Subtitle numberOfLines={2}>{item.body}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    );
+  };
   return (
     <View style={style.container}>
-      <View>
-        <Text>This Is Post</Text>
+      <Text>This Is Post</Text>
 
-        <Button
-          title="To Detail Post"
-          onPress={() => props.navigation.navigate("DetailPost")}
-        />
-        {/* Render Component List */}
-      </View>
+      <Button title="To Detail Post" onPress={() => props.navigation.navigate("DetailPost")} />
+      <FlatList
+        data={post}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={_renderItem}
+        refreshing={isLoading}
+        onRefresh={getPost}
+        nestedScrollEnabled
+      />
     </View>
   );
 };

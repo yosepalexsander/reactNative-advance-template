1ebdd937e65c19cadb330ec94caf4b878add948d
yosep <yosepalexsander9@gmail.com>
2021-11-18 11:13:22 +0700

    done passing params to route

--- src/screens/detailSoc.js
+++ src/screens/detailSoc.js
@@ -2,18 +2,11 @@ import * as React from "react";
 import { Text, Box } from "native-base";
 
 // Declare route
-export default function DetailSoc({}) {
+export default function DetailSoc({ route }) {
   return (
-    <Box
-      safeArea
-      bg="primary.400"
-      flex={1}
-      alignItems="center"
-      justifyContent="center"
-      p={10}
-    >
+    <Box safeArea bg="primary.400" flex={1} alignItems="center" justifyContent="center" p={10}>
       <Text fontFamily="body" fontWeight={400} fontStyle="italic" fontSize={30}>
-        {/* Implement Params to get Data on previous screen  */}
+        {route.params.name || "Not Found"}
       </Text>
     </Box>
   );
--- src/screens/listSoc.js
+++ src/screens/listSoc.js
@@ -4,19 +4,21 @@ import { Text, Box, FlatList, Pressable } from "native-base";
 // Add Props in Hello({navigation})
 export default function Hello({ navigation }) {
   // Set Dummy Data with Array
-
+  const socialMedia = ["YouTube", "Instagram", "Netflix"];
   // Make Function handle press to get value per list
-
+  const handlePress = (value) => {
+    navigation.navigate("Detail Social", { name: value });
+  };
   return (
-    <Box
-      safeArea
-      bg="primary.400"
-      flex={1}
-      alignItems="center"
-      justifyContent="center"
-      p={10}
-    >
-      {/* Render Array With Flatlist */}
+    <Box safeArea bg="primary.400" flex={1} alignItems="center" justifyContent="center" p={10}>
+      <FlatList
+        data={socialMedia}
+        renderItem={({ item }) => (
+          <Pressable key={item} onPress={() => handlePress(item)}>
+            <Text>{item}</Text>
+          </Pressable>
+        )}
+      />
     </Box>
   );
 }
